6b646b06d02157c105d508ec63d9f5085069a4c7
Benjamin Legrand <benjamin.legrand.externe@capgemini.com>
2015-11-13 10:42:54 +0100

    finalized codevember #12 : seven segment clock.

--- src/2015/11/12/12.js
+++ src/2015/11/12/12.js
@@ -6,7 +6,7 @@
  - e   c
  -   d
  */
-//
+
 /**
  * Array of equivalence  between digit/ binary bars to light
  * example : index of the tab : binary abcdefg to display in hexadecimal when called with .toString(2)
@@ -16,25 +16,27 @@
  * @type {number[]}
  */
 const DIGIT_TO_HEX = [
-        0x7E,
-        0x30,
-        0x6D,
-        0x79,
-        0x33,
-        0x5B,
-        0x5F,
-        0x70,
-        0x7F,
-        0x7B
-    ];
-    /**
-     * Returns a binary representation of the seven segment bars to light.
-     *
-     * @param i
-     * @returns {string}
-     */
+    0x7E,
+    0x30,
+    0x6D,
+    0x79,
+    0x33,
+    0x5B,
+    0x5F,
+    0x70,
+    0x7F,
+    0x7B
+];
+/**
+ * Returns a binary representation of the seven segment bars to light.
+ *
+ * @param i the digit to display (index of the array)
+ * @returns {string} 7 bits in a string.
+ */
 const barToLightFromInt = (i)=> {
-        return DIGIT_TO_HEX[i].toString(2);
+        let s = DIGIT_TO_HEX[i].toString(2);
+        while (s.length < 7) s = "0" + s;
+        return s;
     },
     /**
      * Adds a leading 0 to an integer (for instance, takes 2, returns '02')
@@ -51,7 +53,7 @@ const barToLightFromInt = (i)=> {
      * @param test
      */
     toggleOnOff = (element, test)=> {
-        if (test) {
+        if (test === '1') {
             element.classList.remove('off');
             element.classList.add('on');
         } else {
@@ -62,7 +64,7 @@ const barToLightFromInt = (i)=> {
     applyToDOMElement = (elem, digit) => {
         const digits = barToLightFromInt(digit);
         for (let i = 0; i < digits.length; i++) {
-            toggleOnOff(elem.children[i],digits[i]);
+            toggleOnOff(elem.children[i], digits[i]);
         }
     },
     refreshDate = ()=> {
@@ -71,29 +73,31 @@ const barToLightFromInt = (i)=> {
             min = trimDigit(date.getMinutes()),
             sec = trimDigit(date.getSeconds());
 
-        for(let i = 0;i<hour.length;i++){
-            applyToDOMElement(document.getElementById('h-'+(i+1)),hour[i]);
+        for (let i = 0; i < hour.length; i++) {
+            applyToDOMElement(document.getElementById('h-' + (i + 1)), hour[i]);
         }
-        for(let i = 0;i<min.length;i++){
-            applyToDOMElement(document.getElementById('m-'+(i+1)),min[i]);
+        for (let i = 0; i < min.length; i++) {
+            applyToDOMElement(document.getElementById('m-' + (i + 1)), min[i]);
         }
-        for(let i = 0;i<sec.length;i++){
-            applyToDOMElement(document.getElementById('s-'+(i+1)),sec[i]);
+        for (let i = 0; i < sec.length; i++) {
+            applyToDOMElement(document.getElementById('s-' + (i + 1)), sec[i]);
         }
-        window.setTimeout(()=>refreshDate, 1000);
+        requestAnimationFrame(refreshDate);
+        //window.setTimeout(()=>refreshDate, 1000);
     },
     drawBackground = ()=> {
         const canvas = document.getElementById('canvas'),
             context = canvas.getContext('2d');
         for (let y = 0; y < canvas.height; y++) {
             for (let x = 0; x < canvas.width; x++) {
-                const color = Math.floor(Math.random() * 100),
-                    width = Math.floor(Math.random() * 10)+1;
+                const color = Math.floor(Math.random() * 10),
+                    width = Math.floor(Math.random() * 10) + 1;
                 context.fillStyle = `rgb(${color},${color},${color})`;
                 context.fillRect(x, y, width, 1);
                 x += (width - 1);
             }
         }
+        //requestAnimationFrame(drawBackground);
     },
     resize = () => {
         const canvas = document.getElementById('canvas');
