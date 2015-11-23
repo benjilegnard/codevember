e20238fba5320e5da31fba58e16d88e78b1f19f7
Benjamin Legrand <benjilegnard@protonmail.com>
2015-11-23 17:49:18 +0100

    codevember 20/21/22/23

--- src/2015/11/21/21.js
+++ src/2015/11/21/21.js
@@ -1,78 +1,78 @@
-
-
 const LINE_COLOR = 'white';
 const BACK_COLOR = 'rgb(0,0,0)';
 const SOUND_SOURCE = 'http://media.soundcloud.com/stream/vOY0tiM1S6pU.mp3';
-var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-var gainNode = audioContext.createGain();
-var analyser = audioContext.createAnalyser();
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
+const analyser = audioContext.createAnalyser();
 
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
 analyser.fftSize = 512;
 var buffer = null;
 var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 
-function playSound(buffer){
-    let source = audioContext.createBufferSource(); // creates a sound source
-    source.buffer = buffer;
-    source.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-    source.connect(analyser);
-    source.loop = true;
-    source.start();
-}
+const
+    playSound =
+        (buffer) => {
+            let source = audioContext.createBufferSource(); // creates a sound source
+            source.buffer = buffer;
+            source.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+            source.connect(analyser);
+            source.loop = true;
+            source.start();
+        },
+    draw =
+        () => {
+            analyser.getByteFrequencyData(dataArray);
 
-function draw(){
-    analyser.getByteFrequencyData(dataArray);
-
-    var baseY = canvas.height/2,
-        path = new Path2D();
-    path.moveTo(0,baseY);
-    var spaceX = canvas.width / bufferLength;
-    for(let i = 0; i < bufferLength; i++) {
-        let y = dataArray[i]/128 * baseY - baseY;
-        path.lineTo(
-            i*spaceX,
-            y+baseY);
-    }
-    context.strokeStyle = LINE_COLOR;
-    context.stroke(path);
-}
-
-function animate(timestamp){
-    context.fillStyle = BACK_COLOR;
-    context.clearRect(0,0,canvas.width,canvas.height);
-    context.fillRect(0,0,canvas.width,canvas.height);
-    //Draw
-    draw();
-    requestAnimationFrame( animate );
-}
-
-function resize(event){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-function loaded(event){
-    audioContext.decodeAudioData(this.response, playSound);
-}
-
-function requestSource(){
-    let request = new XMLHttpRequest();
-    request.responseType = "arraybuffer";
-    request.addEventListener("load", loaded);
-    request.open("GET", SOUND_SOURCE);
-    request.send();
-}
-
-function initialize(event){
-    requestSource();
-    resize();
-    animate();
-}
+            var baseY = canvas.height / 2,
+                path = new Path2D();
+            path.moveTo(0, baseY);
+            var spaceX = canvas.width / bufferLength;
+            for (let i = 0; i < bufferLength; i++) {
+                let y = dataArray[i] / 128 * baseY - baseY;
+                path.lineTo(
+                    i * spaceX,
+                    y + baseY);
+            }
+            context.strokeStyle = LINE_COLOR;
+            context.stroke(path);
+        },
+    animate = (timestamp)=> {
+        context.fillStyle = BACK_COLOR;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw
+        draw();
+        requestAnimationFrame(animate);
+    },
+    resize =
+        (event)=> {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        },
+    loaded =
+        (event) => {
+            audioContext.decodeAudioData(this.response, playSound);
+        },
+    requestSource =
+        ()=> {
+            let request = new XMLHttpRequest();
+            request.responseType = "arraybuffer";
+            request.addEventListener("load", loaded);
+            request.open("GET", SOUND_SOURCE);
+            request.send();
+        },
+    initialize =
+        (event)=> {
+            requestSource();
+            resize();
+            animate();
+        };
 
 window.addEventListener('resize', resize);
 document.addEventListener('DOMContentLoaded', initialize);
--- src/2015/11/24/24.js
+++ src/2015/11/24/24.js
@@ -1,1 +1,78 @@
- 
+const LINE_COLOR = 'white';
+const BACK_COLOR = 'rgb(0,0,0)';
+const SOUND_SOURCE = 'http://media.soundcloud.com/stream/vOY0tiM1S6pU.mp3';
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
+const analyser = audioContext.createAnalyser();
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+analyser.fftSize = 512;
+var buffer = null;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+const
+    playSound =
+        (buffer) => {
+            let source = audioContext.createBufferSource(); // creates a sound source
+            source.buffer = buffer;
+            source.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+            source.connect(analyser);
+            source.loop = true;
+            source.start();
+        },
+    draw =
+        () => {
+            analyser.getByteFrequencyData(dataArray);
+
+            var baseY = canvas.height / 2,
+                path = new Path2D();
+            path.moveTo(0, baseY);
+            var spaceX = canvas.width / bufferLength;
+            for (let i = 0; i < bufferLength; i++) {
+                let y = dataArray[i] / 128 * baseY - baseY;
+                path.lineTo(
+                    i * spaceX,
+                    y + baseY);
+            }
+            context.strokeStyle = LINE_COLOR;
+            context.stroke(path);
+        },
+    animate = (timestamp) => {
+        context.fillStyle = BACK_COLOR;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw
+        draw();
+        requestAnimationFrame(animate);
+    },
+    resize =
+        (event) => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        },
+    loaded =
+        (event) => {
+            audioContext.decodeAudioData(this.response, playSound);
+        },
+    requestSource =
+        ()=> {
+            let request = new XMLHttpRequest();
+            request.responseType = "arraybuffer";
+            request.addEventListener("load", loaded);
+            request.open("GET", SOUND_SOURCE);
+            request.send();
+        },
+    initialize =
+        (event) => {
+            requestSource();
+            resize();
+            animate();
+        };
+
+window.addEventListener('resize', resize);
+document.addEventListener('DOMContentLoaded', initialize);
