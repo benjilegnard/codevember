0d150999f1a323072fd411da34a1e25db1aeb390
Benjamin Legrand <benjamin.legrand.externe@capgemini.com>
2015-11-24 18:16:01 +0100

    codevember 24

--- src/2015/11/24/24.js
+++ src/2015/11/24/24.js
@@ -1,78 +1,133 @@
-const LINE_COLOR = 'white';
-const BACK_COLOR = 'rgb(0,0,0)';
-const SOUND_SOURCE = 'http://media.soundcloud.com/stream/vOY0tiM1S6pU.mp3';
-
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const gainNode = audioContext.createGain();
-const analyser = audioContext.createAnalyser();
-
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-analyser.fftSize = 512;
-var buffer = null;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-
-const
-    playSound =
-        (buffer) => {
-            let source = audioContext.createBufferSource(); // creates a sound source
-            source.buffer = buffer;
-            source.connect(gainNode);
-            gainNode.connect(audioContext.destination);
-            source.connect(analyser);
-            source.loop = true;
-            source.start();
-        },
-    draw =
-        () => {
-            analyser.getByteFrequencyData(dataArray);
-
-            var baseY = canvas.height / 2,
-                path = new Path2D();
-            path.moveTo(0, baseY);
-            var spaceX = canvas.width / bufferLength;
-            for (let i = 0; i < bufferLength; i++) {
-                let y = dataArray[i] / 128 * baseY - baseY;
-                path.lineTo(
-                    i * spaceX,
-                    y + baseY);
-            }
-            context.strokeStyle = LINE_COLOR;
-            context.stroke(path);
-        },
-    animate = (timestamp) => {
-        context.fillStyle = BACK_COLOR;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        //Draw
-        draw();
-        requestAnimationFrame(animate);
+const COLORS = {
+        white: 0xFFFFFF,
+        pastelBlue: 0xC9E6FF
     },
-    resize =
-        (event) => {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        },
-    loaded =
-        (event) => {
-            audioContext.decodeAudioData(this.response, playSound);
-        },
-    requestSource =
-        ()=> {
-            let request = new XMLHttpRequest();
-            request.responseType = "arraybuffer";
-            request.addEventListener("load", loaded);
-            request.open("GET", SOUND_SOURCE);
-            request.send();
-        },
-    initialize =
-        (event) => {
-            requestSource();
-            resize();
-            animate();
-        };
+    TAU = Math.PI * 2,
 
-window.addEventListener('resize', resize);
-document.addEventListener('DOMContentLoaded', initialize);
+    FLAKE_BRANCHES = 3,
+    FLAKE_ROOTS = 6,
+    FLAKE_LENGTH = 4,
+
+    FALL_VARIATION = TAU / 12,
+    FALL_SPEED = 1,
+    TREE_MAX_HEIGHT = window.innerHeight,
+    context = document.getElementById('snowflakes');
+
+class Snowflake {
+    constructor(props) {
+        this.position = {};
+        this.position.x = props.x;
+        this.position.y = props.y;
+        this.position.z = props.z;
+        this.radius = FLAKE_LENGTH / 1;
+        this.rotation = props.rotation;
+        this.direction = -TAU / 4 + Math.random() * FALL_VARIATION;
+    }
+
+    move(frameCount) {
+        this.direction = Math.sin(frameCount) * FALL_VARIATION;
+        this.position.x += Math.cos(this.direction) * FALL_SPEED;
+        this.position.y += Math.sin(this.direction) * FALL_SPEED;
+    }
+
+    draw() {
+        context.strokeStyle = 'white';
+        let branchDirection = this.direction;
+        for (let i = 0; i < FLAKE_ROOTS; i++) {
+            context.beginPath();
+            context.moveTo(
+                this.position.x,
+                this.position.y
+            );
+            context.lineTo(
+                this.position.x + Math.cos(branchDirection) * this.radius,
+                this.position.y + Math.sin(branchDirection) * this.radius);
+            context.stroke();
+            branchDirection += TAU / FLAKE_ROOTS;
+        }
+    }
+
+    reset() {
+        if (this.y > canvas.height) {
+            this.y = 0 - this.radius;
+        }
+    }
+}
+
+class Tree {
+    constructor(props) {
+        this.x = props.x;
+        this.y = props.y;
+        this.z = props.z;
+        this.color = '#98CDDB';
+    }
+
+    reset(x, y, z) {
+
+    }
+
+    draw() {
+        context.moveTo(this.x, this.y - this.z * .5);
+        context.lineTo()
+    }
+
+    fall() {
+
+    }
+}
+
+class Scene {
+
+}
+
+const mousePosition = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+    },
+    displacementVector = {
+        x: 0,
+        y: 0
+    },
+    parallaxFactor = 10,
+//from farthest to closest
+    layers = ['veryBackgroundTrees', 'backgroundTrees', 'middleTrees', 'foreGround', 'groundAndGrass'],
+
+    applyTransform = (displacementVector) => {
+        var x, y, z, index;
+        for (index = 0; index < layers.length; index++) {
+            z = index + 1;
+            x = (z * displacementVector.x * parallaxFactor);
+            y = (z * displacementVector.y * parallaxFactor) / 3;
+            document.getElementById(layers[index])
+                .setAttribute('transform', 'translate(' + x + ', ' + y + ')');
+        }
+    },
+    /**
+     * transform the mouse position to a vector.
+     * returns -1 -1, if the mouse it at top left of the windows)
+     */
+    calculateVector = (mousePosition) => {
+        displacementVector.x = mousePosition.x / window.innerWidth * 2 - 1;
+        displacementVector.y = mousePosition.y / window.innerHeight * 2 - 1;
+    },
+    /**
+     * Mouse move event handler
+     */
+    mouseMove = (event) => {
+
+        //event.stopPropagation();
+        mousePosition.x = event.clientX;
+        mousePosition.y = event.clientY;
+
+        calculateVector(mousePosition);
+        applyTransform(displacementVector);
+    }
+    ,
+    /**
+     * Touch move event handler
+     */
+    touchMove = (event) => {
+        for (var i = 0; i < event.touches.length; i++) {
+            mouseMove(event.touches[i]);
+        }
+    }
